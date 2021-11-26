8ae3b0fcc2adcbcdf9b894bab652fe34393690d2
Jan Van Ryckeghem <jan@tabs-spaces.be>
2021-11-26 16:23:17 +0100

    Tests for adding a player

--- src/test/kotlin/com/adaptionsoft/games/uglytrivia/GameTest.kt
+++ src/test/kotlin/com/adaptionsoft/games/uglytrivia/GameTest.kt
@@ -1,6 +1,7 @@
 package com.adaptionsoft.games.uglytrivia
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchThrowable
 import org.junit.jupiter.api.Test
 
 class GameTest {
@@ -23,6 +24,37 @@ class GameTest {
             assertThat(rockQuestions).containsExactly(
                 *(0..49).map { "Rock Question $it" }.toTypedArray()
             )
+        }
+    }
+
+    @Test
+    fun `Add a player`() {
+        val game = Game()
+
+        val playerName = "player"
+        val actual = game.add(playerName)
+
+        assertThat(actual).isTrue()
+        with(game) {
+            assertThat(players).containsExactly(playerName)
+            assertThat(places[0]).isEqualTo(0)
+            assertThat(purses[0]).isEqualTo(0)
+            assertThat(inPenaltyBox[0]).isFalse()
+        }
+    }
+
+    @Test
+    fun `Has at most 6 players`() {
+        val game = Game()
+
+        val thrown = catchThrowable {
+            repeat(7) { playerNumber ->
+                game.add("player-$playerNumber")
+            }
+        }
+
+        assertThat(thrown).isInstanceOfSatisfying(ArrayIndexOutOfBoundsException::class.java) {
+            assertThat(it).hasMessage("Index 6 out of bounds for length 6")
         }
     }
 }
