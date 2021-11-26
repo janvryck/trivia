5e006d1bac80f9e5d87ad1f6dc3b51770bd3ac1e
Jan Van Ryckeghem <jan@tabs-spaces.be>
2021-11-26 15:53:15 +0100

    Verify game initialization

--- src/test/kotlin/com/adaptionsoft/games/uglytrivia/GameTest.kt
+++ src/test/kotlin/com/adaptionsoft/games/uglytrivia/GameTest.kt
@@ -6,7 +6,23 @@ import org.junit.jupiter.api.Test
 class GameTest {
 
     @Test
-    fun `Can create Game`() {
-        assertThat(Game()).isNotNull
+    fun `Can initialize Game`() {
+        val game = Game()
+
+        assertThat(game).isNotNull
+        with(game) {
+            assertThat(popQuestions).containsExactly(
+                *(0..49).map { "Pop Question $it" }.toTypedArray()
+            )
+            assertThat(scienceQuestions).containsExactly(
+                *(0..49).map { "Science Question $it" }.toTypedArray()
+            )
+            assertThat(sportsQuestions).containsExactly(
+                *(0..49).map { "Sports Question $it" }.toTypedArray()
+            )
+            assertThat(rockQuestions).containsExactly(
+                *(0..49).map { "Rock Question $it" }.toTypedArray()
+            )
+        }
     }
 }
